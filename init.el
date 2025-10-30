2e9f3aa3f404eb5df7ed5671bb8403c27691a0cf
John Buckley <jbuckleyv@gmail.com>
2025-10-30 13:07:07 -0500

    feat: configure flymake

--- init.el
+++ init.el
@@ -282,3 +282,17 @@
   (setq eldoc-echo-area-display-truncation-message nil)
   :init
   (global-eldoc-mode))
+
+;;; FLYMAKE
+;; Flymake is an on-the-fly syntax checking extension that provides real-time feedback
+;; about errors and warnings in your code as you write. This can greatly enhance your
+;; coding experience by catching issues early. The configuration below activates
+;; Flymake mode in programming buffers.
+(use-package flymake
+  :ensure nil          ;; This is built-in, no need to fetch it.
+  :defer t
+  :hook (prog-mode . flymake-mode)
+  :custom
+  (flymake-margin-indicators-string
+   '((error "!»" compilation-error) (warning "»" compilation-warning)
+     (note "»" compilation-info))))
