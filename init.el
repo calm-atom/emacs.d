71848abdd5bf695a3f2e63c2e5bb22fb357194b5
John Buckley <jbuckleyv@gmail.com>
2025-10-31 07:29:54 -0500

    feat: add treesit-auto

--- init.el
+++ init.el
@@ -469,3 +469,18 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode)) ;; Enable preview in Embark collect mode.
 
+;;; TREESITTER-AUTO
+;; Treesit-auto simplifies the use of Tree-sitter grammars in Emacs,
+;; providing automatic installation and mode association for various
+;; programming languages. This enhances syntax highlighting and
+;; code parsing capabilities, making it easier to work with modern
+;; programming languages.
+(use-package treesit-auto
+  :ensure t
+  :straight t
+  :after emacs
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode t))
