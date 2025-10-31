cc07ca913cc52943bd067bad0c3d6bb04cff9748
John Buckley <jbuckleyv@gmail.com>
2025-10-31 07:30:37 -0500

    feat: add markdown mode

--- init.el
+++ init.el
@@ -484,3 +484,15 @@
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode t))
+
+;;; MARKDOWN-MODE
+;; Markdown Mode provides support for editing Markdown files in Emacs,
+;; enabling features like syntax highlighting, previews, and more.
+;; It’s particularly useful for README files, as it can be set
+;; to use GitHub Flavored Markdown for enhanced compatibility.
+(use-package markdown-mode
+  :defer t
+  :straight t
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)            ;; Use gfm-mode for README.md files.
+  :init (setq markdown-command "multimarkdown")) ;; Set the Markdown processing command.
